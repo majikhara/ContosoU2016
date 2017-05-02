2de048d36110c44e0b1bc0a7b41e3164acfaa0f6
Emily Allain <emilyjacquelineallain@gmail.com>
2017-05-02 12:22:09 -0300

    Added Annotations to Model

--- ContosoU2016/Models/Instructor.cs
+++ ContosoU2016/Models/Instructor.cs
@@ -1,14 +1,19 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Threading.Tasks;
 
 namespace ContosoU2016.Models
 {
+
     public class Instructor : Person
     {
+        [DataType(DataType.Date)]
+        [DisplayFormat(DataFormatString = "{0:yyyy-MM-dd}", ApplyFormatInEditMode = true)]
+        [Display(Name = "Hire Date")]
         public DateTime HireDate { get; set; }
-        
+
         //================== NAVIGATION PROPERTIES ==================
         //public virtual ICollection<CourseAssignment> Courses { get; set; }
         //public virtual OfficeAssignment OfficeAssignment { get; set; }
--- ContosoU2016/Models/Person.cs
+++ ContosoU2016/Models/Person.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -9,12 +10,23 @@ namespace ContosoU2016.Models
     {
         //eallain: Create the data models
         public int ID { get; set; }
+
+        [Required]
+        [StringLength(65, ErrorMessage ="Last Name cannot be longer than 65 characters!")]
+        [Display(Name ="Last Name")]
         public string LastName { get; set; }
+
+        [Required]
+        [StringLength(50, ErrorMessage = "First Name cannot be longer than 50 characters!")]
+        [Display(Name ="First Name")]
         public string FirstName { get; set; }
+
+        [DataType(DataType.EmailAddress)]
         public string Email { get; set; }
 
         //FullName: Calculated Property with a get accessor only
         //          *will not get generated in in database
+        [Display(Name ="Name")]
         public string FullName
         {
             get
